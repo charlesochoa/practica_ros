7da399fb5bc4c9fc7013fed0a7ac904a207a778a
charlesochoa <ing.charlesochoa@gmail.com>
2021-05-02 16:29:12 +0200

    comunication done. Plot still to go

--- src/ex2_rendezvous/scripts/rendezvous_robot_naive.py
+++ src/ex2_rendezvous/scripts/rendezvous_robot_naive.py
@@ -11,50 +11,50 @@ import numpy as np
 import tf
 
 class Robot():
-    def __init__(self, robot_id, x0, y0, rate, robots_ids):
+    def __init__(self, robot_id, x0, y0, Tlocal, robots_ids):
         # Class attributes 
         self.robot_id = robot_id
         self.x = x0
         self.y = y0
         self.pub = rospy.Publisher('topic_queue_position_plot', queue_position_plot, queue_size=10)
+        self.set_neig(robots_ids)
         self.ser = rospy.Service('gossip_update'+str(self.robot_id), gossip_update, self.handle_gossip_update)
-        self.act_dummy(rate, robots_ids)
+        print(self.neig_id)
+        self.act_dummy(Tlocal)
 
-    def act_dummy(self, rate, robots_ids):
-        rate = rospy.Rate(1.0/rate)
-        if (self.robot_id == 0):
-            neig_id = 1
-            rospy.wait_for_service('gossip_update'+str(neig_id)) # ask for service gossip_update
-            try:
-                print("I request a service")
-                service_gossip_update = rospy.ServiceProxy('gossip_update'+str(neig_id), gossip_update)
-                resp1=service_gossip_update(self.robot_id, self.x, self.y)
-                print('Reply received: (', resp1.avg_x, ',', resp1.avg_y)
+    def set_neig(self, robots_ids):
+        count = 0
+        ne_tmp = -1
+        if not np.amax(robots_ids) <= self.robot_id:
+            while ne_tmp <= self.robot_id and count < 10000:
+                r = np.random.randint(0, len(robots_ids))
+                ne_tmp = robots_ids[r]
+        self.neig_id = ne_tmp
 
-            except rospy.ServiceException as e:
-                print("Service call failed: %s"%e)
+    def act_dummy(self, Tlocal):
 
-            print("I publish at topic_queue_position_plot")
-            my_pos_plot=queue_position_plot()
-            my_pos_plot.robot_id=self.robot_id
-            my_pos_plot.x=self.x
-            my_pos_plot.y=self.y
-            self.pub.publish(my_pos_plot)
-        #Up the here, endif
+        rate = rospy.Rate(Tlocal)
+        print(Tlocal)
 
         while not rospy.is_shutdown():
+            if (self.neig_id > -1):
+                rospy.wait_for_service('gossip_update'+str(self.neig_id)) # ask for service gossip_update
+                try:
+                    print("I request a service")
+                    service_gossip_update = rospy.ServiceProxy('gossip_update'+str(self.neig_id), gossip_update)
+                    resp1=service_gossip_update(self.robot_id, self.x, self.y)
+                    print('Reply received: (', resp1.avg_x, ',', resp1.avg_y)
+                    self.x = resp1.avg_x
+                    self.y = resp1.avg_y
 
+                except rospy.ServiceException as e:
+                    print("Service call failed: %s"%e)
             my_pos_plot=queue_position_plot()
             my_pos_plot.robot_id=self.robot_id
             my_pos_plot.x=self.x
             my_pos_plot.y=self.y
-            print("I request a service")
-            service_gossip_update = rospy.ServiceProxy('gossip_update'+str(robots_ids[0]), gossip_update)
-            resp1=service_gossip_update(self.robot_id, self.x, self.y)
-            print('Reply received: (', resp1.avg_x, ',', resp1.avg_y)
             self.pub.publish(my_pos_plot)
-            hello_str = "hello world %s" % rospy.get_time()
-            rospy.loginfo(hello_str)
+            rospy.loginfo(my_pos_plot)
             rate.sleep()
 
     def handle_gossip_update(self, req):
@@ -76,9 +76,9 @@ if __name__ == '__main__':
         robot_id=0
     try:
         rospy.init_node('deploy_'+str(robot_id), anonymous=False)
-        robots_ids = sysargv[5:]
+        robots_ids = [ int(arg_str) for arg_str in sysargv[5:] ]
         print(robots_ids)
-        my_naive_robot=Robot(robot_id, float(sysargv[2]), float(sysargv[3]), int(sysargv[4]), robots_ids)
+        my_naive_robot=Robot(int(robot_id), float(sysargv[2]), float(sysargv[3]), int(sysargv[4]), robots_ids)
         print('Finished!')
     except rospy.ROSInterruptException:
         pass
